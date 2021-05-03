16b3e7ddf1a94a3b6eea0c619045133bb76ba5d0
shoogoome <shoogoome@sina.com>
2021-05-03 09:16:43 +0800

    init

---
+++ module/account.go
@@ -0,0 +1,12 @@
+package module
+
+// 账户表
+type Account struct {
+	ID         int    `gorm:"primary_key" json:"id"`    // 主键
+	Email      string `json:"email"`                    // 邮箱
+	Nickname   string `json:"nickname" gorm:"not null"` // 昵称
+	Username   string `json:"username" gorm:"not null"` // 用户名
+	Password   string // 密码
+	CreateTime int64  `json:"create_time"` // 创建时间
+	UpdateTime int64  `json:"update_time"` // 更新时间
+}
---
+++ module/blog.go
@@ -0,0 +1,13 @@
+package module
+
+// 博客表
+type Account struct {
+	ID         int    `gorm:"primary_key" json:"id"`             // 主键
+	AuthorID   int    `json:"author_id"`                         // 作者id
+	Title      string `json:"title" gorm:"not null"`             // 标题
+	Content    string `json:"content" gorm:"type:text;not null"` // 正文
+	Draft      bool   `json:"draft" gorm:"default:true"`         // 是否草稿
+	Private    bool   `json:"private" gorm:"default:false"`      // 是否私有
+	CreateTime int64  `json:"create_time"`                       // 创建时间
+	UpdateTime int64  `json:"update_time"`                       // 更新时间
+}
